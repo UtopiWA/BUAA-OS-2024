690dfab2a1f311c6d8dde41cccecae39830cf73f
王培鑫 <22371490@buaa.edu.cn>
2024-03-06 21:56:28 +0800

    3.6第三题提交1

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
+
+#grep -n "$2" "$1" > "$3"
+#awk -F: '{print $1}' "$3" >> "$3"
+
+grep -n "$2" "$1" | awk -F:  '{print $1}' >> "$3"
