d44edaa80282eea6b5734e93ef45d69faa966bed
王培鑫 <22371490@buaa.edu.cn>
2024-03-14 19:13:07 +0800

    lab1提交2

--- tools/readelf/readelf.c
+++ tools/readelf/readelf.c
@@ -55,7 +55,9 @@ int readelf(const void *binary, size_t size) {
 		unsigned int addr;
 		/* Exercise 1.1: Your code here. (2/2) */
 		shdr = (Elf32_Shdr *)sh_table;	
-		addr = (*shdr).sh_addr + i * sh_entry_size;		
+		// addr = (*shdr).sh_addr + i * sh_entry_size;
+		addr = (*shdr).sh_addr;
+		sh_table += sh_entry_size;
 
 
 		printf("%d:0x%x\n", i, addr);
