4f65152dacf066295bf73de6dad09b739572ff53
王培鑫 <22371490@buaa.edu.cn>
2024-04-24 18:50:54 +0800

    lab3提交7

--- kern/sched.c
+++ kern/sched.c
@@ -52,4 +52,20 @@ void schedule(int yield) {
 		env_run(e);
 	}
 
+	
+	//if ((yield != 0) || (count == 0) || (e == NULL) || (e->env_status != ENV_RUNNABLE)) {
+	//	if (e) {
+	//		TAILQ_REMOVE(&env_sched_list, e, env_sched_link);
+	//		if (e->env_status == ENV_RUNNABLE) {
+	//			TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
+	//		}
+	//	}
+	//	if (TAILQ_EMPTY(&env_sched_list)) {
+	//		panic("no runnable envs!");
+	//	}
+	//	e = TAILQ_FIRST(&env_sched_list);
+	//	count = e->env_pri;
+	//}
+	//count--;	
+	//env_run(e);
 }
