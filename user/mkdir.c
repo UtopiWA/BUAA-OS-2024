0c6b897192d8b90f1580c3134a180d10fe61647b
王培鑫 <22371490@buaa.edu.cn>
2024-06-19 13:04:25 +0800

    fin mkdir

--- user/mkdir.c
+++ user/mkdir.c
@@ -21,9 +21,28 @@ int main(int argc, char **argv) {
 		if ((fd = open(argv[0], O_RDONLY)) >= 0) {
 			return 0;
 		}
-		if (create(argv[0], FTYPE_DIR) < 0) {
-			// 递归创建目录
+
+		int cnt = 0;
+		char name[32][MAXNAMELEN];
+		strcpy(name[0], argv[0]);
+
+		while (create(name[cnt], FTYPE_DIR) < 0) { // 向上层找到能创建的目录
+			int len = strlen(name[cnt]);
+			for (; len >= 0 && name[cnt][len] != '/'; len--)
+				;
+			if (len < 0) {
+				return 0;
+			}
+			strcpy(name[cnt + 1], name[cnt]);
+			name[++cnt][len] = '\0';		
 		}
+		cnt--;
+		for (; cnt >= 0; cnt--) { // 再依次向下创建目录
+			if (create(name[cnt], FTYPE_DIR) < 0) {
+				return 0;
+			}
+		}
+
 	}
 
 	return 0;
