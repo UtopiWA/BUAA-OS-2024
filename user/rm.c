56028c073e85af3f45b803e1c1edbb0024374e45
王培鑫 <22371490@buaa.edu.cn>
2024-06-22 02:13:45 +0800

    fix rm

--- user/rm.c
+++ user/rm.c
@@ -1,51 +1,7 @@
 #include <lib.h>
 
 int flag[256];
-/*
-char *strcat(char *dest, const char *src)
-{
-	char *tmp = dest;	 
-	while (*dest)
-		dest++;
-	while ((*dest++ = *src++) != '\0')
-		;
-	return tmp;
-}
 
-int rmloop(char *path) {	
-	int fd, type;
-	struct Filefd *ffd;
-	struct File *dir;
-
-	fd = open(path, O_RDONLY);
-	ffd = (struct Filefd *)num2fd(fd);
-	dir = &(ffd->f_file);
-	type = dir->f_type;
-	if (type == FTYPE_REG) {
-		return remove(path);
-	} else { // 递归删除
-		char name[MAXNAMELEN];
-		strcpy(name, path);
-
-		u_int nblock = dir->f_size / BLOCK_SIZE;
-		for (int i = 0; i < nblock; i++) {
-			void * blk;
-			try(file_get_block(dir, i, &blk));
-			struct File *files = (struct File *)blk;
-
-			for (struct File *f = files; f < files + FILE2BLK; ++f) {
-				if (f->f_name) {
-					strcat(name, "/");
-					strcat(name, f->f_name);
-					rmloop(name);
-				}
-			}
-		}
-		return remove(path);
-	}
-	return 0;
-}
-*/
 int remove_loop(char *path) { // imitate 'dir_lookup' in 'fs/fs.c'
 	int fd, type;
 	struct Filefd *ffd;
@@ -90,9 +46,3 @@ int main(int argc, char **argv) {
 
 	return 0;
 }
-/*
-#include <lib.h>
-int main() {
-	return 0;
-}
-*/
